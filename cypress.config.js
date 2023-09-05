4128080e49095491867b7f890afe32ee12c0713f
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-09-05 13:24:37 +0300

    init

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectId: "tvxs1k"
+}
+module.exports = {
+  projectId: 'tvxs1k',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
